602302fda665c929806e6950b9e0605a6722cdde
maurorodriguez <mauroalejo2002@hotmail.com>
2022-06-22 18:57:46 -0300

    react redux setup

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,18 @@
+import {} from '../actions/actionTypes'
+
+const initialState = {
+
+}
+
+export function reducer(state = initialState, {type, payload }){
+
+    switch (type) {
+        // case value:
+            
+        //     break;
+    
+        default:
+            break;
+    }
+
+}
