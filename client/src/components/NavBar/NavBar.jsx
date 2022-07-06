3a02e1c0dc085128f32197e5c62d32ef56235d4a
maurorodriguez <mauroalejo2002@hotmail.com>
2022-07-06 13:31:50 -0300

    Added back button in NavBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import styles from './NavBar.module.css';
+import backImage from '../../images/back.png';
 
 function Navbar() {
+  const { pathname } = useLocation();
+  const history = useHistory();
+
+  const handleBack = () => {
+    history.goBack();
+  };
+
   return (
     <div id={styles.navbar}>
-      <SearchBar />
-      <Link className={styles.createRecipe} to="/recipe/create">
-        CREATE
-      </Link>
+      <button id={styles.backBtn} type="button" onClick={handleBack}>
+        <img src={backImage} alt="" />
+      </button>
+      {pathname === '/home' && <SearchBar />}
+      {pathname !== '/recipe/create' && (
+        <Link className={styles.createRecipeBtn} to="/recipe/create">
+          CREATE
+        </Link>
+      )}
     </div>
   );
 }
