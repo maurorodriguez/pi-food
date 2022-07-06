0d4dc16a1167ba2f4dc2752859edf9e04359258b
maurorodriguez <mauroalejo2002@hotmail.com>
2022-07-06 13:30:03 -0300

    added NotFound component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import MainRoute from './components/MainRoute/MainRoute';
 import DetailRecipe from './components/DetailRecipe/DetailRecipe';
 import CreateRecipe from './components/CreateRecipe/CreateRecipe';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/home" exact component={MainRoute} />
-      <Route path="/recipe/detail/:id" exact component={DetailRecipe} />
-      <Route path="/recipe/create" exact component={CreateRecipe} />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/home" exact component={MainRoute} />
+        <Route path="/recipe/detail/:id" exact component={DetailRecipe} />
+        <Route path="/recipe/create" exact component={CreateRecipe} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NotFound.module.css';
+
+function NotFound() {
+  return (
+    <div id={styles.notFound}>
+      <h1>ERROR 404: PAGE NOT FOUND</h1>
+    </div>
+  );
+}
+
+export default NotFound;
