8997a48fc32c627863b12d55538c3b77c778ddd7
maurorodriguez <mauroalejo2002@hotmail.com>
2022-07-06 13:31:10 -0300

    Added dish types

--- client/src/components/DetailRecipeCard/DetailRecipeCard.jsx
+++ client/src/components/DetailRecipeCard/DetailRecipeCard.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 import styles from './DetailRecipeCard.module.css';
 
 function DetailRecipeCard({ recipe }) {
-  const { name, summary, image, diets, healthScore, instructions } = recipe;
+  const { name, summary, image, diets, healthScore, instructions, dishTypes } =
+    recipe;
   const defaultImage =
     'https://us.123rf.com/450wm/gioiak2/gioiak21708/gioiak2170800312/84667691-signo-de-interrogaci%C3%B3n-rojo-en-un-plato-sobre-fondo-negro-ilustraci%C3%B3n-3d.jpg';
   return (
@@ -22,11 +23,25 @@ function DetailRecipeCard({ recipe }) {
         <div id={styles.diets}>
           <h2>Diets:</h2>
           {diets &&
-            diets.map((d) => <h3 key={d.name || d}>{d.name || d} - </h3>)}
+            diets.map((d, index) => (
+              <h3 key={d.name || d}>
+                {d.name || d} {index !== diets.length - 1 ? '-' : ''}
+              </h3>
+            ))}
         </div>
         <h2>Instructions:</h2>
         {instructions &&
-          instructions.map((i, index) => <h3>{`${index + 1}: ${i}`}</h3>)}
+          instructions.map((text, index) => (
+            <h3 key={text}>{`${index + 1}: ${text}`}</h3>
+          ))}
+        <div id={styles.dishTypes}>
+          {dishTypes &&
+            dishTypes.map((dishType, index) => (
+              <h3 key={dishType}>
+                {dishType} {index !== dishTypes.length - 1 ? '- ' : ''}
+              </h3>
+            ))}
+        </div>
       </div>
     </div>
   );
