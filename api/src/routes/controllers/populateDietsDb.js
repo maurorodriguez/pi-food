38d16f6be1b9a5fbb496aea68ea056c848e659f4
maurorodriguez <mauroalejo2002@hotmail.com>
2022-07-06 13:48:30 -0300

    identation

--- api/src/routes/controllers/populateDietsDb.js
+++ api/src/routes/controllers/populateDietsDb.js
@@ -1,29 +1,31 @@
-const {Diet} = require('../../db');
-const axios = require('axios')
+const { Diet } = require('../../db');
+const axios = require('axios');
 require('dotenv').config();
-const {API_KEY} = process.env;
+const { API_KEY } = process.env;
 
-const populateDietsDb = async(req, res, next) => {
-    try {
-        const diets = await Diet.findAll();
-        if (!diets.length) {
-            const rawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&number=100&apiKey=${API_KEY}`)
-            rawData.data.results.forEach(recipe => {
-                recipe.diets.forEach(async (diet) => {
-                    await Diet.findOrCreate({
-                        where:{
-                            name: diet
-                        }
-                    })
-                })
-            });
-            return res.status(201).send('Diets have been successfully created');
-        }
-
-        return res.status(200).send(diets)
-    } catch (error) {
-        next(error);
+const populateDietsDb = async (req, res, next) => {
+  try {
+    const diets = await Diet.findAll();
+    if (!diets.length) {
+      const rawData = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&number=100&apiKey=${API_KEY}`
+      );
+      rawData.data.results.forEach((recipe) => {
+        recipe.diets.forEach(async (diet) => {
+          await Diet.findOrCreate({
+            where: {
+              name: diet,
+            },
+          });
+        });
+      });
+      return res.status(201).send('Diets have been successfully created');
     }
-}
 
-module.exports = populateDietsDb
+    return res.status(200).send(diets);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = populateDietsDb;
